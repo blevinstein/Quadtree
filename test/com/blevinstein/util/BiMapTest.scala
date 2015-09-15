e1d9c70f09c0d49e1e90dea74c6ff76a0fcdab2f
Brian Levinstein <blevinstein@gmail.com>
2015-09-15 11:09:03 -0400

    Add BiMapTest

---
+++ test/com/blevinstein/util/BiMapTest.scala
@@ -0,0 +1,16 @@
+package com.blevinstein.util
+
+import org.scalatest._
+
+class BiMapTest extends FunSuite with Matchers {
+  test("BiMap basics") {
+    val b = new BiMap("a" -> 1, "b" -> 2)
+    b("a") shouldEqual 1
+    b.inverse(2) shouldEqual "b"
+  }
+  test("BiMap same type") {
+    val b = new BiMap(1 -> 2, 2 -> 3)
+    b(1) shouldEqual 2
+    b.inverse(3) shouldEqual 2
+  }
+}
