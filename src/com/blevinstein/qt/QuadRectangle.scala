f8e7cd351602ac23a94df1d7aa9e2a6c62e9a413
Brian Levinstein <blevinstein@gmail.com>
2015-09-18 00:29:42 -0400

    Implement QuadRectangle#prune

--- src/com/blevinstein/qt/QuadRectangle.scala
+++ src/com/blevinstein/qt/QuadRectangle.scala
@@ -8,6 +8,30 @@ class QuadRectangle(val min: QuadOffset, val max: QuadOffset) {
       new Point(1f * min.x / (1 << min.depth), 1f * min.y / (1 << min.depth)),
       new Point(1f * max.x / (1 << max.depth), 1f * max.y / (1 << max.depth)))
 
+  // Returns the intersection of two QuadRectangles
+  // TODO implement test
+  def prune(other: QuadRectangle): QuadRectangle = {
+    def min2d(a: QuadOffset, b: QuadOffset): QuadOffset = {
+      QuadOffset.normalize(a, b) match {
+        case (maxDepth, aNorm, bNorm) =>
+          new QuadOffset(maxDepth,
+            math.min(aNorm.x, bNorm.x),
+            math.min(aNorm.y, bNorm.y)).simplify
+      }
+    }
+    def max2d(a: QuadOffset, b: QuadOffset): QuadOffset = {
+      QuadOffset.normalize(a, b) match {
+        case (maxDepth, aNorm, bNorm) =>
+          new QuadOffset(maxDepth,
+            math.max(aNorm.x, bNorm.x),
+            math.max(aNorm.y, bNorm.y)).simplify
+      }
+    }
+    new QuadRectangle(max2d(min, other.min), min2d(max, other.max))
+  }
+
+  // TODO def allAddresses(depth: Int): List[QuadAddr] = {
+
   // Operators
 
   def +(offset: QuadOffset): QuadRectangle =
