42a8a4ecc38c4fd6a4f00b6deeb82aff931b049d
Brian Levinstein <blevinstein@gmail.com>
2015-11-04 00:55:22 -0800

    Add createDirectory to Config

--- src/com/blevinstein/util/Config.scala
+++ src/com/blevinstein/util/Config.scala
@@ -4,6 +4,10 @@ import java.io.IOException
 import java.nio.file._
 
 class Config(pathPrefix: String = "config") {
+  if (!Files.exists(Paths.get(pathPrefix))) {
+    Files.createDirectory(Paths.get(pathPrefix))
+  }
+
   def get(filename: String): Option[String] = {
     try {
       Some(new String(Files.readAllBytes(Paths.get(pathPrefix, filename))))
