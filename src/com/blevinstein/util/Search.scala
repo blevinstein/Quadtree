dc90a05f09d9720c585462a434c3a52deb942f19
Brian Levinstein <blevinstein@gmail.com>
2015-11-05 23:37:22 -0800

    Implement floodfill and simple test.

---
+++ src/com/blevinstein/util/Search.scala
@@ -0,0 +1,33 @@
+package com.blevinstein.util
+
+import scala.collection.mutable.Stack
+import scala.collection.mutable.HashSet
+
+/**
+ * This class contains implementations of generic search algorithms, such as
+ * BFS/DFS, floodfill, or A*.
+ */
+object Search {
+  def floodfill[Node](start: Node, neighbors: Node => Iterable[Node]):
+      List[Node] = {
+    val visited = new HashSet[Node]()
+    val stack = new Stack[Node]()
+    var result = List[Node]()
+
+    stack.push(start)
+    visited.add(start)
+
+    while (!stack.isEmpty) {
+      val current = stack.pop()
+      for (neighbor <- neighbors(current)) {
+        if (!visited.contains(neighbor)) {
+          stack.push(neighbor)
+          visited.add(neighbor)
+        }
+      }
+      result = current :: result
+    }
+
+    result
+  }
+}
---
+++ test/com/blevinstein/util/SearchTest.scala
@@ -0,0 +1,15 @@
+package com.blevinstein.util
+
+import org.scalatest._
+
+class SearchTest extends FunSuite with Matchers {
+  test("floodfill") {
+    // Search space: integers 1-10
+    // Use floodfill to get even/odd numbers
+    def neighbors(num: Int): List[Int] =
+      for (n <- List(num-2, num+2) if 0 < n && n <= 10) yield n
+
+    Search.floodfill(1, neighbors).toSet shouldEqual Set(1, 3, 5, 7, 9)
+    Search.floodfill(8, neighbors).toSet shouldEqual Set(2, 4, 6, 8, 10)
+  }
+}
